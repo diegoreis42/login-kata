9870bc34f240cb5488437f05cb2e2ffe3400721e
diegoreis42 <d2022014641@unifei.edu.br>
2024-01-23 05:56:14 -0300

    basic setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DomainsModule } from './domains/domains.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(DomainsModule);
   await app.listen(3000);
 }
 bootstrap();
